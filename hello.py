df6531f55dd9b16135de4520672dd5aaa053e79e
Muhammad Hussein <hoseennet1@gmail.com>
2023-02-12 14:08:36 +0200

    g

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello , world!")
+n = int(input("enter number : "))
+if n > 0:
+    print("number is positve")
+elif n < 0:
+    print("number is negative")
+else:
+    print("number is zero")
